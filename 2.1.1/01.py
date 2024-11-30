bbc0b9399a493f17d49183f2f33ebf7bbca54510
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-11-30 21:11:46 +0530

    module 2.1.1

---
+++ 2.1.1/01.py
@@ -0,0 +1,21 @@
+# Formula -> dividend - (math.floor(dividend/divisor)*divisor)
+
+# a=0; b=7
+# print("Reminder of {} and {} is {}".format(a, b,a%b))
+
+a=7; b=0
+print("Reminder of {} and {} is {}".format(a, b,a%b))
+
+
+# What is the value of -11%-2?
+# a. -1
+# b. 0
+# c. 1
+# d. 2
+
+
+# What is the output of 0%-2?
+# a. 0
+# b. -2
+# c. 2
+# d. Error
