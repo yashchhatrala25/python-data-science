6a79d08bc10f4d8085ad43fb0aea0a5717bcc0b2
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-11-30 17:13:24 +0530

    module 1.2.3 complete

---
+++ 1.2.3/01.py
@@ -0,0 +1,17 @@
+# Editing, Saving and Running Scripts
+
+source_file = open("g:\\sample_Writing.txt", "w")
+print("Writing to a file", file = source_file)
+source_file.close()
+
+# Which among the following does not help to create a virtual environment?
+# a. virtualenv
+# b. pipenv
+# c. conda
+# d. pypi
+
+# Which among the following is incorrect way of executing a script?
+# a. python filename.py
+# b. python filename.py > output.txt
+# c. python filename.py >> output.txt
+# d. python > filename.py output.txt
