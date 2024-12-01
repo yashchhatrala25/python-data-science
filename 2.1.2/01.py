82049b2038708cce29446b90e50a540d4c487f17
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-12-01 17:42:15 +0530

    module 2.1.2

---
+++ 2.1.2/01.py
@@ -0,0 +1,12 @@
+# Which of following is not a decision-making statement.
+# a. if
+# b. if…else
+# c. if…elif…else
+# d. if...else…elif
+
+
+# What keyword would you use to add an alternative condition to an if statement?
+# a. if_else
+# b. elseif
+# c. else if
+# d. elif
