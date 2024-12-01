ff2467224330f607b078ee98961b860a1cee9121
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-12-01 18:17:31 +0530

    module 2.1.3

---
+++ 2.1.3/01.py
@@ -0,0 +1,46 @@
+# Relational Operators
+# a=7; b=0; c=-7
+# print("Positive : {}, Zero : {}, Negative : {}".format(bool(a), bool(b), bool(c)))
+
+# Boolean Expression - Relational Operators
+# a=7; b=0
+# print(a<b)
+# print(a!=b)
+
+
+#Relational Operators
+# a=7; b=5; c=4; d=1; e=5
+# if a>b>c>d and e>c:
+#     print("a is greatest")
+# else:
+#     print("a is not the greatest")
+
+# Logical Operators - not
+# a=7; b=10
+# if not a>b:
+#     print("b is greatest")
+
+# Logical Operators - and
+# a=7; b=5; c=3
+# if a>b and a>c:
+#     print("a is greatest")
+
+# Logical Operators - or
+a=50; b=45
+if a>=50 or b>45:
+    print("Gread : B")
+
+# if 2 - 3 = -1:
+#     print("Equal")
+# else:
+#     print("Unequal")
+# a. Equal
+# b. Unequal
+# c. Compiled Successfully, No Output.
+# d. Syntax Error
+
+# What are Boolean expressions composed of?
+# a. arithmetic and logical operators
+# b. relational and logical operators
+# c. arithmetic and relational operators
+# d. membership and relational operators
